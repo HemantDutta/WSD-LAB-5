44cd444c0b813ae2ef6d22339c3fffc099f8af9b
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-02 18:33:58 +0530

    nav, form, footer and animations

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+function loadAnimations(){
+    let nav = document.getElementById('nav');
+    nav.style.transform = "translateY(0%)";
+    nav.style.opacity = "100%"
+    let form = document.getElementById('form');
+    form.style.transform = "translateY(0%)";
+    form.style.opacity = "100%"
+    
+}
